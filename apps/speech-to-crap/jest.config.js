9aa295f3bedcd3332f7b5b6aa693527ccedf8a1e
Ajit Singh <ajit.singh@jambit.com>
2020-12-12 13:14:28 +0100

    initial commit

---
+++ apps/speech-to-crap/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'speech-to-crap',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/speech-to-crap',
+};
